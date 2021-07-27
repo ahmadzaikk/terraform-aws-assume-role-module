62504674df7fee833e32df047f5107d59d59276d
Khalid <33844176+ahmadzaikk@users.noreply.github.com>
2021-07-27 15:12:29 -0700

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -3,6 +3,11 @@ variable "name" {
   description = "The name of the role. If omitted, Terraform will assign a random, unique name."
 }
 
+variable "name" {
+  type        = string
+  description = "The name of the role. If omitted, Terraform will assign a random, unique name."
+}
+
 variable "assume_role_policy" {
   type        = string
   description = "The policy that grants an entity permission to assume the role."
